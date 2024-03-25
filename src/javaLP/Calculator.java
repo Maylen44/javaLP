2cc569767b0ff1f8fa3e90e3fc7b9e3b0a51eced
Maylen44 <belz1993@bk.ru>
2024-03-25 20:03:29 +0100

    finished bonus task 3 and normal taskes up to 12

--- src/javaLP/Calculator.java
+++ src/javaLP/Calculator.java
@@ -16,12 +16,48 @@ public class Calculator
 		System.out.println(m_fieldValue);
 		System.out.println(m_fieldValue2);
 	}
+	
 	int sum(int lw, int rw)
 	{
 		int result = lw + rw;
 		return result;
 	}
 	
+	public static String calculateIBAN(String country, String bankCode, String accountNumber)
+	{
+		String countryCode = convertCountryCode(country);
+		String intermediateValue = bankCode + accountNumber + countryCode + "00";
+		long modulo = calculateModulo(intermediateValue);
+		int checkDigit = (int) (98 - modulo);
+		return country + String.format("%02d", checkDigit) + " " + 
+		bankCode.substring(0, 4) + " " + 
+		bankCode.substring(4) + " " + 
+		accountNumber.substring(0, 4) + " " +
+		accountNumber.substring(4) + " " + 
+		String.format("%02d", checkDigit);
+	}
+	
+	public static String convertCountryCode(String country)
+	{
+		StringBuilder countryCode = new StringBuilder();
+		for(char c : country.toCharArray())
+		{
+			int numericValue = Character.getNumericValue(c);
+			countryCode.append(numericValue > 9 ? numericValue : "0" + numericValue);
+		}
+		return countryCode.toString();
+	}
+	
+	public static long calculateModulo(String value)
+	{
+		long remainder = 0;
+		for (int i = 0; i < value.length(); i += 7)
+		{
+			long number = Long.parseLong(remainder + value.substring(i, Math.min(i + 7, value.length())));
+			remainder = number % 97;
+		}
+		return remainder;
+	}
 
 	
 	private int m_fieldValue;
--- src/javaLP/Main.java
+++ src/javaLP/Main.java
@@ -1,24 +1,30 @@
 package javaLP;
-import static javax.swing.JOptionPane.*;	
+import static javax.swing.JOptionPane.*;
 import java.util.Scanner;
 
 public class Main 
 {
+	
+	/*
 	//so, enums are not int valued, but as members of enum type. we can specifie there value like this MyEnum_One(value), like MyEnum_One(5) has value 5
 	enum MyEnum
 	{
 		MyEnum_One,
 		MyEnum_Two,
 		MyEnum_Three,
-	}
+	} 
+	*/
+	
 	
 	
 	public static void main(String[] args) 
 	{
-		System.out.print(MyEnum.MyEnum_One);
+		
+
 		//testing inclusion of other classes in different files
 		//Calculator calc = new Calculator();
 		//System.out.print(calc.sum(9, 43) + "\n");
+		//System.out.print(MyEnum.MyEnum_One);
 		
 		/*
 		//test code
@@ -136,7 +142,124 @@ public class Main
 		System.out.printf("%11.0f%11.2f%11.4e%n", zahl, zahl, zahl);
 		//Out right on 20 postision, filled with 0, dez 6
 		System.out.printf("%020.6f%n", zahl);
+		
+		
+		//BAufgabe 3
+		//I: Land = 2 char gross into 4 int, BLZ = 8 int, Kontonummer = 10stellig
+		//O: IBAN
+		String country = "DE";
+		String benkCode = "12345678";
+		String accountNumber = "0000987654";
+		
+		String iban = Calculator.calculateIBAN(country, benkCode, accountNumber);
+		System.out.println("IBAN: " + iban);
+		
+		//Aufgabe 6
+		double pi = Math.PI;
+		System.out.println(pi);
+		//auf 3 Stelle gerundet
+		System.out.printf("%.3f\n", pi);
+		//auf 8 Stelle gerundet
+		System.out.printf("%.8f\n", pi);
+		//*1.000.000 as expo Number
+		System.out.printf("%e\n", pi * 1000000);
+		//no dez and position 10
+		System.out.printf("%10.0f\n", pi);
+		//bonus
+		System.out.printf("Das Doppelte von %.1f ist %.1f\n", 3.33, 3.33 * 2);
+		
+		//Aufgabe 7
+		byte ganzeZahl = 5;
+		double dezimalZahl = 2.73;
+		String text = "Die FOM";
+		boolean wahrheit = false;
+		char buchstabe = 'A';
+		System.out.println(ganzeZahl);
+		System.out.println(dezimalZahl);
+		System.out.println(text);
+		System.out.println(wahrheit);
+		System.out.println(buchstabe);
+		//buchstabe++;
+		System.out.println(buchstabe++);
+		//++buchstabe;
+		System.out.println(++buchstabe);
+		buchstabe += 1;
+		System.out.println(buchstabe);
+		ganzeZahl += 123;
+		System.out.println(ganzeZahl);
+		//doesn't work.
+		//byte zahl123 = 123;
+		//ganzeZahl = ganzeZahl + zahl123;
+		//System.out.println(ganzeZahl);
+		ganzeZahl = (byte) (ganzeZahl + 123);
+		System.out.println(ganzeZahl);
+		System.out.println(text + " ist toll");
+		String text2 = " ist toll";
+		System.out.println(text + text2);
+		
+		//Aufgabe 8
+		float input = Float.parseFloat(showInputDialog("Zahl eingeben: "));
+		System.out.printf("%f * 2 = %f \n", input, input * 2);
+		
+		//Aufgabe 9
+		double radius = Double.parseDouble(showInputDialog("Zahl eingeben: "));
+		double pi = Math.PI;
+		double umfang = 2 * pi * radius;
+		double flaeche = pi * (radius * radius);
+		System.out.printf("Bei einem Radius von %.2f betraegt der Umfang %.2f  Die Flaeche ist %.2f\n", radius, umfang, flaeche);
+		
+		//Aufgabe 10
+		int j;
+		int i = 5;
+		System.out.println(i + 3 * 4); //17
+		System.out.println(i * 3 + 1 * 2); //17
+		System.out.println(i % 5 * 4 / 2); //0
+		System.out.println(i % (5 * 4 / 2)); //0 5%10? = 5 als Restwert
+		System.out.println(i++ - i * i); //5 - 6 * 6 = 5 - 36 = -31
+		i = 5;
+		System.out.println(- ++i + - --i - + i++); //-6 - 5 - 5 = -16
+		i = 5;
+		System.out.println(- ++i + - (--i - + i++)); //-6 - (5 - 5) = -6
+		i = 5;
+		System.out.println(- --i + - i++ % 2 + i++ % 2 * 10 / 2); //-4 - 4 % 2 + 5 % 2 * 10 / 2 = -4 - 0 + 1 * 10 / 2 = 1
+		i = 5;
+		System.out.println(- --i + - (i++ % 2 + i++ % 2) * 10 / 2); //-4 - (4 % 2 + 5 % 2) * 10 / 2 = -4 - 5 = -9
+		
+		//Aufgabe 11
+		Scanner scan = new Scanner(System.in);
+		int laenge = Integer.parseInt(showInputDialog("Laenge: "));
+		int breite = Integer.parseInt(showInputDialog("Breite: "));
+		int hoehe = Integer.parseInt(showInputDialog("Hoehe: "));
+		int oberflaeche = laenge * breite * hoehe;
+		int volumen = 2 * laenge * breite + 2 * laenge * hoehe + 2 * breite * hoehe;
+		System.out.printf("Ein Quader mit der Laenge %d cm, der Breite %d cm und der Hoehe %d cm besitzt eine Oberflaeche von %d cm^2 und ein Volumen von %d cm^3.\n", laenge, breite, hoehe, oberflaeche, volumen);
+		
+		System.out.println("Laenge: ");
+		laenge = scan.nextInt();
+		System.out.println("Breite: ");
+		breite = scan.nextInt();
+		System.out.println("Hoehe: ");
+		hoehe = scan.nextInt();
+		oberflaeche = laenge * breite * hoehe;
+		volumen = 2 * laenge * breite + 2 * laenge * hoehe + 2 * breite * hoehe;		
+		System.out.printf("Ein Quader mit der Laenge %d cm, der Breite %d cm und der Hoehe %d cm besitzt eine Oberflaeche von %d cm^2 und ein Volumen von %d cm^3.\n", laenge, breite, hoehe, oberflaeche, volumen);
+		
+		//Aufgabe 12
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Vierstellige Zahl eingeben: ");
+		int input = scan.nextInt();
+		int firstDiget = input % 10;
+		int secondDiget = input % 100 / 10;
+		int thirdDiget = input % 1000 / 100;
+		int fourthDiget = input % 10000 / 1000;
+		int result = firstDiget + secondDiget + thirdDiget + fourthDiget;
+		System.out.println(result);
 		*/
+		
+		//Aufgabe 13
+		
+		
+		
 		
 		
 		
